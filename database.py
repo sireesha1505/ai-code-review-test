55ea500ff292c869b70aa46df033f1cd5c373ce4
sireeshajelli1505-code <sireeshajelli1505@gmail.com>
2026-09-13 08:59:52 +0530

    Add additional functions in database.py file

--- database.py
+++ database.py
@@ -1,3 +1,24 @@
 def get_user(name):
     query = "SELECT * FROM users WHERE name = '" + name + "'"
     return query
+
+
+def get_users(names):
+    users = []
+
+    for name in names:
+        query = "SELECT * FROM users WHERE name = '" + name + "'"
+        users.append(query)
+
+    return users
+
+
+def process_users(users):
+    results = []
+
+    for user in users:
+        for other_user in users:
+            if user["id"] == other_user["id"]:
+                results.append(user)
+
+    return results
